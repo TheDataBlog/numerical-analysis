940a319c03b5ca931a423340b7c602e66c1feb21
Kaleb Asefa <kalebasefa0@gmail.com>
2025-11-15 20:42:21 +0300

    change the project to react

--- backend/main.py
+++ backend/main.py
@@ -3,24 +3,29 @@ st.set_page_config(page_title='home', layout='wide')
 st.markdown('<h2 style="text-align: center;">Numerical Methods using streamlit webapp</h2>', unsafe_allow_html=True)
 st.markdown('<h4 style="text-align: center;">A guide for your complex formulas</h4>',  unsafe_allow_html=True)
 st.divider()
-with st.container(horizontal_alignment='center', width='stretch'):
+
+col1, col2, col3 = st.columns(spec=[1,2,1])
+with col2.container(horizontal_alignment='center', width='stretch'):
     method = st.selectbox(label='choose a method',width=600,
              options=['bisection method', 
                     'newton\'s ralph method',
                     'secant method',
                     'fixed point iteration method'])
-    if method == 'bisection method':
+
+    if method in ['bisection method', 'secant method']:
         with st.form(key= 'bisection_method_form', width=600):
+            col1, col2 = st.columns(spec=(12,1))
             formula = st.text_input(label='Enter your formula')
-            col1, col2 = st.columns(spec=[1,1])
-            col1.number_input(label= 'x1')
-            col1.number_input(label='number of iteration', step=1)
-            col2.number_input(label= 'x2')
-            col2.number_input(label='tolerance value')
+            col3, col4 = st.columns(spec=[1,1])
+            col3.number_input(label= 'x1')
+            col3.number_input(label='number of iteration', step=1)
+            col4.number_input(label= 'x2')
+            col4.number_input(label='tolerance value')
             st.form_submit_button()
-    elif method == 'newton\'s ralph method':
+    elif method in ['newton\'s ralph method', 'fixed point iteration method']:
         with st.form(key= 'newton_method_form', width=600):
             formula = st.text_input(label='Enter your formula')
+            initial_point = st.number_input(label='initial number')
             col1, col2 = st.columns(spec=[1,1])
             col2.number_input(label='tolerance value')
             col1.number_input(label='number of iteration', step=1)
