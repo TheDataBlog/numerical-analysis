03ab76a2efe9200c15b9d783c58ef89667d6267d
Kaleb Asefa <kalebasefa0@gmail.com>
2025-12-03 21:11:02 +0300

    change the iteration from loop to recurssion

--- backend/main.py
+++ backend/main.py
@@ -2,8 +2,10 @@ import streamlit as st
 import sympy as sp
 import pandas as pd
 st.set_page_config(page_title='home', layout='wide')
-st.markdown('<h2 style="text-align: center;">Numerical Methods using streamlit webapp</h2>', unsafe_allow_html=True)
-st.markdown('<h4 style="text-align: center;">A guide for your complex formulas</h4>',  unsafe_allow_html=True)
+st.markdown('<h2 style="text-align: center;">Numerical Methods using streamlit webapp</h2>', 
+            unsafe_allow_html=True)
+st.markdown('<h4 style="text-align: center;">A guide for your complex formulas</h4>',  
+            unsafe_allow_html=True)
 st.divider()
 
 with st.container(horizontal_alignment='center', width='stretch'):
@@ -20,7 +22,9 @@ with st.container(horizontal_alignment='center', width='stretch'):
             x1 = col3.number_input(label= 'x1', value=None, format="%0.9f")
             i = col3.number_input(label='number of iteration', step=1, value=None)
             x2 = col4.number_input(label= 'x2', value=None, format="%0.9f")
-            fixed_tolerance_value = col4.number_input(label='tolerance value')
+            fixed_tolerance_value = col4.number_input(label='tolerance value', 
+                                                      value=None, 
+                                                      format="%0.13f")
             sumbitted = st.form_submit_button(width='stretch')
     elif method in ['newton\'s ralph method', 'fixed point iteration method']:
         with st.form(key= 'newton_method_form', width=600):
@@ -53,6 +57,7 @@ class calculation:
             self.out_put.update(kwargs)
         df = pd.DataFrame(self.out_put, index=range(1, len(x1_data) + 1, 1))
         return df
+
 class bisection(calculation):
     def __init__(self, formula, x1, x2, i=None, tolerance_value=None):
         super().__init__(formula, x1, x2, i, tolerance_value)
@@ -67,49 +72,67 @@ class bisection(calculation):
         self.x3_answer_data = []
         self.tolerance_value = (self.x2 - self.x1) / 2
         self.tolerance_value_data = []
-    def evaluating_by_iteration(self):
+        self.fixed_tolerance_value = fixed_tolerance_value
+    def solving_the_equation(self):
+        nothing = lambda x, y: 'x1' if x1 is None else 'x2'
         zero = lambda x, y: 'x1' if x == 0 else 'x2'
-        if self.x1 is None or self.x2 is None or self.i is None:
-            return f'enter the '
+        if self.x1 is None or self.x2 is None:
+            return f'enter the {nothing}'
         elif self.x1_answer * self.x2_answer > 0:
             return 'zero is not found in the range'
         elif self.x1_answer * self.x2_answer == 0:
             return f'zero is found on {(zero(self.x1_answer, self.x2_answer))}'
-        elif self.i is not None:
-            for iter in range(self.i):
-                self.x1_data.append(self.x1)
-                self.x2_data.append(self.x2)
-                self.x3_data.append(self.x3)
-                self.x1_answer_data.append(self.x1_answer)
-                self.x2_answer_data.append(self.x2_answer)
-                self.x3_answer_data.append(self.x3_answer)
-                self.tolerance_value_data.append(self.tolerance_value)
-                if self.x3_answer == 0:
-                    break
-                elif self.x1_answer * self.x3_answer > 0:
-                    self.x1 = self.x3
-                    self.x3 = (self.x1 + self.x2) / 2
-                    self.x3_answer = self.variable.subs(self.x, self.x3)
-                    self.x1_answer = self.variable.subs(self.x, self.x1)
-                    self.tolerance_value = (self.x2 - self.x1) / 2
-                else:
-                    self.x2 = self.x3
-                    self.x3 = (self.x1 + self.x2) / 2
-                    self.x3_answer = self.variable.subs(self.x, self.x3)
-                    self.x2_answer = self.variable.subs(self.x, self.x2)
-                    self.tolerance_value = (self.x2 - self.x1) / 2
-            kwargs = {'x3': self.x3_data, 'value at x3': self.x3_answer_data, 'tolerance value': self.tolerance_value_data}
-            bitch  = self.appending(self.x1_data, self.x1_answer_data, self.x2_data, self.x2_answer_data, **kwargs)
-            return bitch
+        elif i is not None:
+            return self.evaluating_by_iteration()
+        else:
+            return self.evaluating_by_tolerance_value()
+    def the_iteration(self, x1, x2, x3, x1_answer, x2_answer, x3_answer, tolerance_value, i = 1):
+        self.x1_data.append(x1)
+        self.x2_data.append(x2)
+        self.x3_data.append(x3)
+        self.x1_answer_data.append(x1_answer)
+        self.x2_answer_data.append(x2_answer)
+        self.x3_answer_data.append(x3_answer)
+        self.tolerance_value_data.append(tolerance_value)
+        if tolerance_value <= self.fixed_tolerance_value or i == self.i:
+            kwargs = {'x3': self.x3_data, 
+                    'value at x3': self.x3_answer_data, 
+                    'tolerance value': self.tolerance_value_data}
+            df  = self.appending(self.x1_data, 
+                                self.x1_answer_data, 
+                                self.x2_data, 
+                                self.x2_answer_data, 
+                                **kwargs)
+            return df
+        if x1_answer * x3_answer > 0:
+            new_x1, new_x2 = x3, x2
+        else:
+            new_x1, new_x2 = x1, x3
+        new_x3 = (new_x1 + new_x2) / 2
+        return self.the_iteration(
+            new_x1, new_x2, new_x3,
+            self.variable.subs(self.x, new_x1),
+            self.variable.subs(self.x, new_x2),
+            self.variable.subs(self.x, new_x3),
+            abs(new_x2 - new_x1) / 2, i + 1)
+    def evaluating_by_iteration(self):
+        return self.the_iteration(self.x1, 
+                                self.x2, 
+                                self.x3, 
+                                self.x1_answer, 
+                                self.x2_answer, 
+                                self.x3_answer, 
+                                self.tolerance_value)
     def evaluating_by_tolerance_value(self):
-        while fixed_tolerance_value > self.tolerance_value:
-            pass
+        return self.the_iteration(self.x1, 
+                                self.x2, 
+                                self.x3, 
+                                self.x1_answer, 
+                                self.x2_answer, 
+                                self.x3_answer, 
+                                self.tolerance_value)
 
-becalculate = bisection(formula, x1, x2, i)
-kwargs = {'hello': [1,4,5]}
-
+becalculate = bisection(formula, x1, x2, i, fixed_tolerance_value)
 if sumbitted:
     if method == 'bisection method':
-        if i is not None:
-            
-            st.write(becalculate.evaluating_by_iteration())
+        st.write(becalculate.solving_the_equation())
